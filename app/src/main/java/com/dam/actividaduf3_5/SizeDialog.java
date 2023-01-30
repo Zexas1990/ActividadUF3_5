a1e4edfb89de1bf006172a4c9e4a156d11242733
DanielFernandez <dfernandezlouro1@gmail.com>
2023-01-30 13:22:38 +0100

    Add class SizeDialog

--- app/src/main/java/com/dam/actividaduf3_5/SizeDialog.java
+++ app/src/main/java/com/dam/actividaduf3_5/SizeDialog.java
@@ -1,8 +1,22 @@
 package com.dam.actividaduf3_5;
 
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class SizeDialog extends DialogFragment{
 //Iniciar el dialogo
+    EditText etSize;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState){
+        View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_size, null);
+    }
+
 
 }
